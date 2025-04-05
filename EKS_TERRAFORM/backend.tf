8f2e7db8cf29ac6d5f1fa03916a393688dbde673
nandhyalalingareddy <120440033+nandhyalalingareddy@users.noreply.github.com>
2025-04-05 13:53:44 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "Hotstar-linga" # Replace with your actual S3 bucket name
+    bucket = "hotstar-lingareddy" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
